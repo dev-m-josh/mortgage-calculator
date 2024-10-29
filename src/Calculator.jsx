c3e3005d63b1481d2fff4ae1d6118932e365733b
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-10-29 17:10:30 +0300

    css styling

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -18,63 +18,77 @@ const Calculator = () => {
 
     const x = Math.pow(1 + interest, payments); //exponentiation 
     const monthly = (principal * x * interest) / (x - 1); //get monthly repayment
+    const monthlyTotal = monthly.toFixed(2)
 
-    setMonthlyPayment(monthly.toFixed(2));
+    setMonthlyPayment(monthlyTotal);
     //total payment
-    const totalRepaymentAmt = monthlyPayment * 12;
-    setTotalRepayment(totalRepaymentAmt.toFixed(2));
-
+    const RepaymentAmount = monthlyTotal * 12;
+    setTotalRepayment(RepaymentAmount.toFixed(2));
   };
+
 
   return (
     <>
-    <div>
+    <div className='calculation'>
+      <div className='heading'>
       <h1>Mortgage Calculator</h1>
-      <div className='input'>
-      <h3>Mortgage Amount</h3>
-      <input
-        type="number"
-        placeholder="Loan Amount"
-        value={loanAmount}
-        onChange={(e) => setLoanAmount(e.target.value)}
-      />
+      <a>Clear All</a>
+      </div>
+      <div className='mortgage-input'>
+        <h3>Mortgage Amount</h3>
+        <div className='input-bar'>
+          <p style={{width:'30px', backgroundColor: 'hsl(61, 70%, 52%)'}}>$</p>
+          <input
+            type="number"
+            value={loanAmount}
+            onChange={(e) => setLoanAmount(e.target.value)}
+          />
+        </div>
       </div>
       <div className='term'>
-      <div className='input'>
-      <h3>Mortgage Term</h3>
-      <input
-        type="number"
-        placeholder="Loan Term (years)"
-        value={loanTerm}
-        onChange={(e) => setLoanTerm(e.target.value)}
-      />
-      </div>
-      <div className='input'>
-      <h3>Interest Rate</h3>
-      <input
-        type="number"
-        placeholder="Annual Interest Rate (%)"
-        value={interestRate}
-        onChange={(e) => setInterestRate(e.target.value)}
-      />
-      </div>
+        <div className='mortgage-input'>
+        <h3>Mortgage Term</h3>
+        <div className='input-bar'>
+          <input
+            type="number"
+            value={loanTerm}
+            onChange={(e) => setLoanTerm(e.target.value)}
+          />
+          <p>years</p>
+        </div>
+        </div>
+        <div className='mortgage-input'>
+          <h3>Interest Rate</h3>
+          <div className='input-bar'>
+            <input
+              type="number"
+              value={interestRate}
+              onChange={(e) => setInterestRate(e.target.value)}
+            />
+            <p>%</p>
+          </div>
+        </div>
       </div>
       <div className='repayment'>
       <h3>Mortgage Type</h3>
       <div className='types'>
-        <input type="checkbox" />
+        <input type="radio" />
         <p>Repayment</p>
       </div>
       <div className='types'>
-        <input type="checkbox" />
+        <input type="radio" />
         <p>Interest Only</p>
       </div>
       </div>
-      <button onClick={calculateMortgage} disabled={!loanAmount || !interestRate || !loanTerm}>Calculate Repayments</button>
+      <button className='calculate' onClick={calculateMortgage} disabled={!loanAmount || !interestRate || !loanTerm}>Calculate Repayments</button>
     </div>
-    <div>
-    {monthlyPayment && <h2>Monthly Payment: ${monthlyPayment}</h2>}
-    {totalRepayment && <h2>Total Repayment: ${totalRepayment}</h2>}
+    <div className='result'>
+        <h2>Your results</h2>
+        <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
+        <div className='amount'>
+          {monthlyPayment && <p>Your monthly payment: <h2>${monthlyPayment}</h2></p>}
+          {totalRepayment && <p>Total you'll repay over the term: <h3>${totalRepayment}</h3></p>}
+        </div>
     </div>
     </>
   );
